d1e23d2b768b0f683d39da15a692181b95e436df
Matt Hood <matthood0@gmail.com>
2021-12-02 11:49:43 +1100

    Fixed my Day 1 broken clock.

--- Matt/day1.ts
+++ Matt/day1.ts
@@ -4,13 +4,22 @@ const depthInfo: number[] = puzzleData
     .split(/\r?\n/)
     .map(n => parseInt(n));
 
-const windowSum = 
-    (arr: number[], window = 1) => arr
-        .slice(0, -1 * window)
-        .map((_, i) => arr[i + window] - arr[i]);
+// I don't think recursion is always a great idea in JS, but hey, this is AoC.
+function partition<T>(arr: T[], size: number, overlap: number, result: T[][] = []): T[][] {
+    console.assert(size > overlap);
+    if(arr.length < size) return result;
+    else return partition(arr.slice(size - overlap),
+                          size, 
+                          overlap,
+                          [...result, arr.slice(0, size)]);
+}       
+const differences = (arr: number[]) => arr.slice(0, -1).map((_, i) => arr[i + 1] - arr[i]);
 const countPositive = (arr: number[]) => arr.filter( n => n > 0 ).length;
+const sumArray = (arr: number[]) => arr.reduce((a, b) => a + b);
 
-console.log(`Test A, want 7: ${countPositive(windowSum(testData))}`)
-console.log(`Test B, want 5: ${countPositive(windowSum(testData, 3))}`)
-console.log(`Result for Part A: ${countPositive(windowSum(depthInfo))}`);
-console.log(`Result for Part B: ${countPositive(windowSum(depthInfo, 3))}`);
+const partA = (data: number[]) => countPositive(differences(data));
+const partB = (data: number[]) => countPositive(differences(partition(data, 3, 2).map(sumArray)));
+console.log(`Test A, want 7: ${partA(testData)}`)
+console.log(`Test B, want 5: ${partB(testData)}`)
+console.log(`Result for Part A: ${partA(depthInfo)}`);
+console.log(`Result for Part B: ${partB(depthInfo)}`);
